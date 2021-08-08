2d372807830a8a054afa029129eb3d74cd010a40
Aphillip22 <amber.rain.phillips@gmail.com>
2021-08-08 14:43:22 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,14 +27,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -52,8 +54,10 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -69,6 +73,13 @@ var fight = function(enemyName) {
   }
 };
 
+// function to generate a random numeric value
+var randomNumber = function() {
+    var value = Math.floor(Math.random() * 21) + 40;
+  
+    return value;
+};
+
 // function to start a new game
 var startGame = function() {
   // reset player stats
@@ -82,7 +93,7 @@ var startGame = function() {
   
         var pickedEnemyName = enemyNames[i];
   
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
   
         fight(pickedEnemyName);
 
@@ -170,6 +181,6 @@ var shopOptionPrompt = window.prompt(
       break;
   }
 };
-
+}
 // start the game when the page loads
 startGame();
